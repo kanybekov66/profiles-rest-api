d5f306f8dabdd385ada29bde109069489899719d
kanybekov66 <azatbekkanybekov@MacBook-Air-Azatbek.local>
2021-01-17 22:38:17 +0600

    Got rid of vagrant

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for users in the system"""
+    email = models.E
